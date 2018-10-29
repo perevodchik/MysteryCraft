a04e55e4763a0f6884978bc044c33163db0875cc
perevodchik <42819976+perevodchik@users.noreply.github.com>
2018-10-29 20:45:39 +0200

    Complete item Mjolnir

--- src/main/java/perevodchik/item/ItemHammer.java
+++ src/main/java/perevodchik/item/ItemHammer.java
@@ -1,21 +1,14 @@
 package perevodchik.item;
 
-import com.google.common.collect.Multimap;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.init.Blocks;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemHammer extends ItemSword {
+public abstract class ItemHammer extends ItemSword {
 
     public ItemHammer(ToolMaterial material) {
         super(material);
@@ -23,19 +16,12 @@ public class ItemHammer extends ItemSword {
 
     public float getDestroySpeed(ItemStack stack, IBlockState state) { return 5.0F; }
 
-    /**
-     * Current implementations of this method in child classes do not use the entry argument beside ev. They just raise
-     * the damage on the stack.
-     */
     public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
     {
         stack.damageItem(1, attacker);
         return true;
     }
 
-    /**
-     * Called when a Block is destroyed using this Item. Return true to trigger the "Use Item" statistic.
-     */
     public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving)
     {
         if ((double)state.getBlockHardness(worldIn, pos) != 0.0D)
@@ -46,9 +32,6 @@ public class ItemHammer extends ItemSword {
         return true;
     }
 
-    /**
-     * Check whether this Item can harvest the given Block
-     */
     public boolean canHarvestBlock(IBlockState blockIn)
     {
         return blockIn.getBlock() == Blocks.STONE;
--- src/main/java/perevodchik/item/unique/Mjolnir.java
+++ src/main/java/perevodchik/item/unique/Mjolnir.java
@@ -1,9 +1,16 @@
 package perevodchik.item.unique;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import perevodchik.item.ItemHammer;
 
 public class Mjolnir extends ItemHammer {
@@ -15,12 +22,44 @@ public class Mjolnir extends ItemHammer {
         this.setCreativeTab(tab);
     }
 
-    /**
-     * Current implementations of this method in child classes do not use the entry argument beside ev. They just raise
-     * the damage on the stack.
-     */
     public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
     {
         return false;
+    }
+
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        BlockPos LightingBoltPos = new BlockPos(playerIn);
+        BlockPos.MutableBlockPos LighttingBoltMutablePos = new BlockPos.MutableBlockPos(LightingBoltPos);
+
+        EnumFacing face = playerIn.getHorizontalFacing();
+
+        int r = 2 + (int) (Math.random() * 5);
+        while(r<2){
+            r = 2 + (int) (Math.random() * 5);
+        }
+        int q = -6 + (int) (Math.random() * 13);
+
+
+        if(face == EnumFacing.NORTH)
+        {
+            LighttingBoltMutablePos.setPos(LightingBoltPos.getX()+q, LightingBoltPos.getY(), LightingBoltPos.getZ()-r);
+        }
+        else if(face == EnumFacing.SOUTH)
+        {
+            LighttingBoltMutablePos.setPos(LightingBoltPos.getX()+q, LightingBoltPos.getY(), LightingBoltPos.getZ()+r);
+        }
+        else if(face == EnumFacing.WEST)
+        {
+            LighttingBoltMutablePos.setPos(LightingBoltPos.getX()-r, LightingBoltPos.getY(), LightingBoltPos.getZ()+q);
+        }
+        else if(face == EnumFacing.EAST)
+        {
+            LighttingBoltMutablePos.setPos(LightingBoltPos.getX()+r, LightingBoltPos.getY(), LightingBoltPos.getZ()+q);
+        }
+
+        EntityLightningBolt bolt = new EntityLightningBolt(worldIn, LighttingBoltMutablePos.getX(), LighttingBoltMutablePos.getY(), LighttingBoltMutablePos.getZ(), false);
+        worldIn.spawnEntity(bolt);
+        return new ActionResult<ItemStack>(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
     }
 }
